834188e0658f854a4013c00e10ea34e341ea8792
Stian Westvig <stian.westvig@gmail.com>
2016-09-10 12:11:41 +0200

    initial commit

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offset;
+
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+
+	// Runs every frame, but after all positions have been updated
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public Rigidbody rigid;
+    public Text countText;
+    public Text winText;
+
+    private int count;
+
+    void Start() {
+        count = 0;
+        rigid = GetComponent<Rigidbody>();
+        setCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rigid.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Pickup")) {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText() {
+        countText.text = "Count: " + count.ToString();
+        if (count > 7) {
+            winText.text = "You win again, biatch";
+        }   
+    }
+}
